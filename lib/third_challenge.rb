ddc06f16660a92c7fda3fcd5f7943d64ae90c9a9
Maayan Kfir <mayankfir@gmail.com>
2018-09-26 18:50:32 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,12 +1,18 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"}
-      :matriarch => {name: "Lady Montague", age: "54"}
-      :hero => {name: "Romeo", age: "15", status: "alive"}
+      :patriarch => {
+        name: "Lord Montague", age: "53"
+      },
+      :matriarch => {
+        name: "Lady Montague", age: "54"
+      },
+      :hero => {
+        name: "Romeo", age: "15", status: "alive"
+      },
       :hero_friends => []
 
-   }, 
+   },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet", age: "50"
@@ -21,5 +27,5 @@ def third_challenge
    }
   }
 
-  
+
 end
